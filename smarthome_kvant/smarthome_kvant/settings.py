e3bbce7fafc3bd38b2d097d11ff38afa04693129
S1r0p <62458912+S1r0p@users.noreply.github.com>
2020-10-25 09:34:49 +0300

    Merge branch 'main' of C:\Users\админ\PycharmProjects with conflicts.

--- smarthome_kvant/smarthome_kvant/settings.py
+++ smarthome_kvant/smarthome_kvant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'remote_control',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
