87859981e51e8f2c8fd57b9c31dc148c9c2334f7
Vadim-py <66944880+Vadim-py@users.noreply.github.com>
2020-11-03 18:23:33 +0300

    temp

--- smarthome_kvant/remote_control/admin.py
+++ smarthome_kvant/remote_control/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import on_off_position
+from .models import on_off_position, temp_on_off
 
 admin.site.register(on_off_position)
+admin.site.register(temp_on_off)
--- smarthome_kvant/remote_control/views.py
+++ smarthome_kvant/remote_control/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import PositionLedForm
-from .models import on_off_position
+from .models import on_off_position, temp_on_off
 
 
 def index(request):
@@ -17,3 +17,14 @@ def led_1(request):
 	b = on_off_position(position=mod)
 	b.save()
 	return render(request, 'Led_1.html')
+
+def temp(request):
+	mod = 0
+	form = temp_on_off.objects.last()
+	if str(form) == '1':
+		mod = 0
+	if str(form) == '0':
+		mod = 1
+	b = temp_on_off(position=mod)
+	b.save()
+	return render(request, 'temp.html')
