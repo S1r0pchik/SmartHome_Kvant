bf61e064e9d21b501194d5d7a663051f08081611
Student <maltsev.valdimir06@mail.ru>
2020-11-07 19:57:03 +0300

    Проверка на подключение контроллера и вкл/выкл светодиодов по состоянию кнопки

--- smarthome_kvant/remote_control/views.py
+++ smarthome_kvant/remote_control/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import PositionLedForm
 from .models import on_off_position
+from . import pyboard
 
 
 def index(request):
@@ -8,6 +9,15 @@ def index(request):
 
 
 def led_1(request):
+	try:
+		pyb = pyboard.Pyboard('COM7', 115200)
+		pyb.enter_raw_repl()
+	except:
+		position = "Ошибка подключения"
+		context = {
+			'led_pos': position,
+		}
+		return render(request, 'Led_1.html', context)
 	if 'dispatch' in request.POST:
 		form = on_off_position.objects.last()
 		if str(form) == '1':
@@ -16,9 +26,26 @@ def led_1(request):
 			mod = 1
 		bd = on_off_position(position=mod)
 		bd.save()
+		try:
+			pyb.exec_("from pyb import Pin")
+			pyb.exec_("p_out = [Pin(i, Pin.OUT_PP) for i in ('D3','D5','D6') ]")
+		except:
+			pass
 		if mod == 0:
-			position = "Лампа сейчас выключена"
+			try:
+				pyb.exec_("p_out[0].off()")
+				pyb.exec_("p_out[1].off()")
+				pyb.exec_("p_out[2].off()")
+				position = "Лампа сейчас выключена"
+			except:
+				pass
 		if mod == 1:
+			try:
+				pyb.exec_("p_out[0].on()")
+				pyb.exec_("p_out[1].on()")
+				pyb.exec_("p_out[2].on()")
+			except:
+				pass
 			position = "Лампа сейчас включена"
 	else:
 		form = on_off_position.objects.last()
