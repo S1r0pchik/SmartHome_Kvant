ec3925ac995c56500b9b6f9dc9b87993ffa23e66
Андрей <ghdg>
2020-11-14 19:26:51 +0300

    багофикс

--- smarthome_kvant/remote_control/models.py
+++ smarthome_kvant/remote_control/models.py
@@ -7,8 +7,8 @@ class on_off_position(models.Model):
     def __str__(self):
        return self.position
 class trm(models.Model):
-    izm1 = models.IntegerFieldField()
-    izm2 = models.IntegerFieldField()
-    izm3 = models.IntegerFieldField()
-    izm4 = models.IntegerFieldField()
-    izm5 = models.IntegerFieldField()
+    izm1 = models.IntegerField()
+    izm2 = models.IntegerField()
+    izm3 = models.IntegerField()
+    izm4 = models.IntegerField()
+    izm5 = models.IntegerField()
