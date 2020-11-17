2b91b835ef6d3cc3af931d8aad2a52b65fbb3c57
Андрей <ghdg>
2020-11-17 19:47:42 +0300

    добавляю rgb

--- smarthome_kvant/remote_control/urls.py
+++ smarthome_kvant/remote_control/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('led_1', views.led_1, name="led_1"),
-    path('termometr', views.termometr, name="termometr")
+    path('termometr', views.termometr, name="termometr"),
+    path('rgb', views.rgb_lamp, name="rgb")
 ]
 
