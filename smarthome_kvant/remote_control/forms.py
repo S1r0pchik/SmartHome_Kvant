fb9979e85e390375e79d90b75345bba63893b0ea
S1r0p <62458912+S1r0p@users.noreply.github.com>
2020-12-19 19:29:23 +0300

    Осталось починить ВХОД и всё!

---
+++ smarthome_kvant/remote_control/forms.py
@@ -0,0 +1,32 @@
+from .models import Termometr, Led, LedName, PosTerm
+from django.forms import ModelForm, TextInput
+
+
+class TempForm(ModelForm):
+    class Meta:
+        model = Termometr
+        fields = ["time", "temp"]
+
+
+class LedForm(ModelForm):
+    class Meta:
+        model = Led
+        fields = ["pos", "number", "name"]
+
+
+class LedNameForm(ModelForm):
+    class Meta:
+        model = LedName
+        fields = ["add_name"]
+        widgets = {
+            'add_name': TextInput(attrs={
+                'placeholder': 'Название',
+                'class': 'input',
+            }),
+        }
+
+
+class PosTermForm(ModelForm):
+    class Meta:
+        model = PosTerm
+        fields = ["pos_term"]
---
+++ smarthome_kvant/remote_control/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('led/<int:num>', views.led_1, name="led_1"),
+    path('termometr', views.termometr, name="termometr"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+]
